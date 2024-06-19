bbb7767a6039e5d1afa92267c45f4420538dff19
Voloshynd <Voloshynd13@gmail.com>
2024-06-19 21:14:29 +0200

    make build

---
+++ index.708ceae4.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in t){var l=document.createElement("li");l.textContent=a,r.append(l),e(l,t[a])}r.childNodes.length&&n.append(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.708ceae4.js.map
